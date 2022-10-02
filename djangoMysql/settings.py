a11c967aa6756dc087e0197616b79baeb8168bb3
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2022-10-02 20:29:55 -0300

    fix: arruma a configuração do banco de dados no arquivo settings.py do projeto

--- djangoMysql/settings.py
+++ djangoMysql/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "djangoMysql.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": BASE_DIR / "djangoDB",
+        "NAME": "djangoDB",
         "USER": "root",
         "PASSWORD": "pyhidrodev",
         "HOST": "localhost",
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATUC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATUC_ROOT = str(BASE_DIR / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
