15a2f332671d09429ca7424f244c225d7d16a597
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2022-10-02 19:45:57 -0300

    chore: configura os templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
+"""
+Configuração das views do projeto
+"""
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    View responsável pela renderização do template index
+    """
+    return render(request, "index.html")
+
+
+def contato(request):
+    """
+    View responsável pela renderização do template contato
+    """
+    return render(request, "contato.html")
+
+
+def produto(request):
+    """
+    View responsável pela renderização do template produto
+    """
+    return render(request, "produto.html")
